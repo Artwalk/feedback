7cc6da85404ffa31e83a61f10ce008279d425519
Artwalk <artwalkx@gmail.com>
2019-07-17 02:54:24 +0800

    Update rnn_layers & num_epochs

--- testing.py
+++ testing.py
@@ -3,5 +3,5 @@ from textgenrnn import textgenrnn
 print('👍')
 textgenrnn('weights/👍.hdf5').generate_samples(prefix="He")
 
-print('👎')
-textgenrnn('weights/👎.hdf5').generate_samples(prefix="He")
+# print('👎')
+# textgenrnn('weights/👎.hdf5').generate_samples(prefix="He")
--- training.py
+++ training.py
@@ -2,8 +2,10 @@ from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
 
-textgen.train_from_file('datasets/👍.txt', num_epochs=1)
+textgen.train_from_file('datasets/👍.txt',
+                        rnn_layers=5,
+                        num_epochs=25, rnn_bidirectional=True)
 textgen.save('weights/👍.hdf5')
 
-textgen.train_from_file('datasets/👎.txt', num_epochs=1)
-textgen.save('weights/👎.hdf5')
+# textgen.train_from_file('datasets/👎.txt', num_epochs=1)
+# textgen.save('weights/👎.hdf5')
